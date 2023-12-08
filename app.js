0658364e1c956e27e6ac1b5711f6730f5001dfa6
abmarrero <a.ernestomarrerol@gmail.com>
2023-12-07 22:46:19 -0500

    webserver finished

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const app = express()
-const port = 8080
+const express = require('express');
+const app = express();
+require('dotenv').config()
+const port = process.env.PORT;
 
-app.get('/',  (req, res) => {
-  res.send('Home')
-})
+app.use(express.static('public'));
+
 app.get('/hello-world',  (req, res) => {
-    res.send('Hello World')
+    res.send('Hello World');
   })
   app.get('*',  (req, res) => {
-    res.send('Page| not found')
+    res.sendFile(__dirname + '/public/404.html');
   })
   app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`);
   })
